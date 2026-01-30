d06f07f31872f2cefc163cd87e287005ed52e71e
Capung01d <168819229+Capung01d@users.noreply.github.com>
2026-01-30 13:25:10 +0700

    Update track.php

--- track.php
+++ track.php
@@ -1,10 +1,21 @@
 <?php
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      $phoneNumber =POST['phone'];
-      $apiKey = 'pk.785fd489929ebc7a439f5ad4f5890cd';
-      $url = "https://api.opencellid.org/geocode?key=$Key&mcc=234&mnc=01cellid=12345";
-      $response = file_get_contents($url);
-      $data = json($response, true);
-      echo "Lokasi: " . $data['city'] . ", . $data['region_name'] . ", " . $data['country'];
-  }
-  ?>
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $phoneNumber = $_GET['phone'];
+   api = 'pk.7185fd489929ebc7a439f5ad4f5890cd';
+
+    // Ganti dengan endpoint yang benar untuk API OpenCellID
+    $url = "https://api.opencid.orggeocode?key=$apiKey&mcc=234&mnc=01&cellid=12345";
+
+    // Proses permintaan API
+    $response = file_get_contents($url);
+    $data = json_decode($response true);
+
+    ($data) {
+        echo "Lokasi: " . $data['city'] . ", " . $data['region_name'] . ", " . $data['country_name'];
+    } else {
+        echo "Tidak dapat mengambil data lokasi.";
+    }
+ else {
+    echoMethod tidak diizinkan.";
+}
+?>
